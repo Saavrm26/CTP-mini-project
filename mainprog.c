a56e881a2ba8d8cede5f5993192e3d6a9de32dca
Saarthak Verma <saarthakverma739@gmail.com>
2022-03-17 17:04:55 +0530

    added res

--- mainprog.c
+++ mainprog.c
@@ -1,3 +1,37 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<string.h>
+// char *line;
+// char* readline(FILE *file){
+//     line=(char *)calloc(1,sizeof(char));
+//     char chr;
+//     fscanf(file,"%c",&chr);
+//     int i=0;
+//     while(chr!='\n'&&chr!=EOF){
+//         line[i]=chr;
+//         fscanf(file,"%c",&chr);
+//         if(chr!='\n'){
+//             line =(char *)realloc(line , strlen(line)+1);
+//             i++;
+//         }
+//     }
+//     return line;
+// }
+
+// int main(){
+//     FILE *restaurant;
+//     restaurant =fopen("restaurants.txt","r");
+//     char* rest_list[10];
+//     for(int i=0;i<10;i++){
+//         rest_list[i]=readline(restaurant);
+//         free(line);
+//     }
+//     fclose(restaurant);
+//     printf("Hello , Meal time\n\nSelect your restaurant\n\n");
+//     for(int i=0;i<10;i++){
+//         printf("%d %s\n\n",i+1,rest_list[i]); 
+//     }
+// }
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -8,6 +42,7 @@ char* readline(FILE *file){
     fscanf(file,"%c",&chr);
     int i=0;
     while(chr!='\n'&&chr!=EOF){
+        
         line[i]=chr;
         fscanf(file,"%c",&chr);
         if(chr!='\n'){
@@ -28,4 +63,8 @@ int main(){
         free(line);
     }
     fclose(restaurant);
+    printf("Hello , Meal time\n\nSelect your restaurant\n\n");
+    for(int i=0;i<10;i++){
+        printf("%d %s\n\n",i+1,rest_list[i]); 
+    }
 }
